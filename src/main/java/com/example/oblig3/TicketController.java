850dff4ebe7c7d020d7e9b4ca1703332dae80488
Godlia <52934844+Godlia@users.noreply.github.com>
2024-04-22 20:49:15 +0200

    last test

--- src/main/java/com/example/oblig3/TicketController.java
+++ src/main/java/com/example/oblig3/TicketController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-public class TicketHandler {
+public class TicketController {
     @Autowired
     TicketRepository ticketRepository;
 
